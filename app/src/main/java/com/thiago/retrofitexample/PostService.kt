d6c2a50973c044a5f674c9eb153eb699a8872099
thiagocaandrade <thiago.carlos.andrade@gmail.com>
2020-07-13 15:10:15 -0300

    example retrofit

---
+++ app/src/main/java/com/thiago/retrofitexample/PostService.kt
@@ -0,0 +1,11 @@
+package com.thiago.retrofitexample
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface PostService {
+
+    @GET("posts")
+    fun list(): Call<List<PostModel>>
+
+}
---
+++ app/src/main/java/com/thiago/retrofitexample/RetrofitClient.kt
@@ -0,0 +1,33 @@
+package com.thiago.retrofitexample
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+//Singleton
+class RetrofitClient private constructor() {
+
+    companion object {
+
+        private lateinit var retrofit: Retrofit
+        private val baseUrl = "https://jsonplaceholder.typicode.com/"
+
+        private fun getRetrofitInstance(): Retrofit {
+
+            val httpClient = OkHttpClient.Builder()
+            if (!::retrofit.isInitialized) {
+                retrofit = Retrofit.Builder()
+                    .baseUrl(baseUrl).client(httpClient.build())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+
+            return retrofit
+        }
+
+        fun <S >createService(serviceClass: Class<S>): S{
+            return  getRetrofitInstance().create(serviceClass)
+
+        }
+    }
+}
